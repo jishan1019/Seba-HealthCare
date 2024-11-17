6ed019ae71b5051cb648604d4155e337a2ef6b24
jishan1019 <imransilamjishan80@gmail.com>
2024-11-17 22:17:28 +0600

    basic server setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,26 @@
-import express from "express";
-
-const app = express();
+import { Server } from "http";
+import app from "./app";
 
 const port = 4000;
 
-app.listen(port, () => {
-  console.log("Server listening on port " + port);
+let server: Server;
+
+async function main() {
+  server = app.listen(port, () => {
+    console.log("Server listening on port " + port);
+  });
+}
+
+main();
+
+process.on("unhandledRejection", () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+
+process.on("uncaughtException", () => {
+  process.exit(1);
 });
